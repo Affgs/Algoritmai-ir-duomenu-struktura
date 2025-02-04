eceeb8b23646bf655d24d9c416e176c71c8dcff7
Agnė Gaižutytė <s057931@ad.viko.lt>
2025-02-04 13:52:38 +0200

    masyvas sukurtas

--- masyvai/main.cpp
+++ masyvai/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int masyvas[];
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> masyvas[i];
+    }
+
+    int MasyvoDydis;
+    cin >> MasyvoDydis;
+
+
+ /*   int suma( int masyvas[], int n) {
+        int suma = 0;
+        for (int i = 0; i < n; i++) {
+            suma = suma + masyvas[i];
+            cout << masyvas[i] << " ";
+            cout << '\n';
+        }
+    }
+*/
+
+
     return 0;
 }
